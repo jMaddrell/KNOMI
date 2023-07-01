b2be70fc60fbe3141851276a8bdd66217f3a75c6
Jordan Maddrell <jordan.maddrell@gmail.com>
2023-07-01 19:15:46 +0100

    Apply auto formatter

--- Firmware/src/lvgl_logo_btt.h
+++ Firmware/src/lvgl_logo_btt.h
@@ -2,19 +2,18 @@
 #define LVGL_LOGO_BTT_H
 
 #ifdef __cplusplus
-extern "C" {  //extern "C"表示编译生成的内部符号名使用C约定。这样在c++文件中也可以调用对应c函数
+extern "C"
+{ // extern "C"表示编译生成的内部符号名使用C约定。这样在c++文件中也可以调用对应c函数
 #endif
 
 #include <lvgl.h>
 
-extern lv_obj_t * img_open_logo;
+    extern lv_obj_t *img_open_logo;
 
-
-void init_img_open_logo_display();
-
+    void init_img_open_logo_display();
 
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
 
-#endif  
+#endif
