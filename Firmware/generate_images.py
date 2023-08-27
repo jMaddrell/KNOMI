bb0947730babebee3d7bea4cbd3455ab2653f9b1
Jordan Maddrell <jordan.maddrell@gmail.com>
2023-08-27 16:30:04 +0100

    Remove hardcoded images, generate them instead and optimise them

--- Firmware/generate_images.py
+++ Firmware/generate_images.py
@@ -1,16 +1,18 @@
 from os import listdir, makedirs
 from os.path import isfile, join, splitext
 from optimise_images import optimise
+import subprocess
 
-Import("env")
+# Import("env")
 
 optimise()
-gifs = [f for f in listdir('GIFS') if isfile(join('GIFS', f))]
+images = [f for f in listdir('images') if isfile(join('images', f))]
 
 makedirs(join("src", "generated"), exist_ok=True)
 
-for gif in gifs:
-    parts = splitext(gif)
+for image in images:
+    parts = splitext(image)
     print("Converting: %s" % {parts[0]})
-    env.Execute("lv_img_conv GIFS/%s%s -f -c CF_RAW_CHROMA -o src/generated/%s.c" %
-                (parts[0], parts[1], parts[0]))
+    input_file = "images/%s%s" % (parts[0], parts[1])
+    output_file = "src/generated/%s.c" % (parts[0])
+    subprocess.call(["lv_img_conv", input_file, "-f", "-c", "CF_RAW_CHROMA", "-o", output_file])
--- Firmware/optimise_images.py
+++ Firmware/optimise_images.py
@@ -1,14 +1,13 @@
 from os import listdir, makedirs
 from os.path import isfile, join, splitext
 import subprocess
+from PIL import Image
 
-# Import("env")
+src = "./images_raw"
+dest = "./images"
 
-src = "./GIFS_Raw"
-dest = "./GIFS"
-
-gifs = [f for f in listdir(src) if isfile(join(src, f))]
-
+gifs = [f for f in listdir(src) if isfile(join(src, f)) and f.endswith('.gif')]
+bmps = [f for f in listdir(src) if isfile(join(src, f)) and not f.endswith('.gif')]
 # sources = list(map(lambda f: join(src, f), gifs))
 
 
@@ -18,6 +17,11 @@ def optimise():
     for gif in gifs:
         subprocess.call(["gifsicle", '--verbose', '--optimize=3', '--lossy=80', join(src, gif),
                         "--colors", '256', "--output", join(dest, gif)])
+        
+    for bmp in bmps:
+        im = Image.open(join(src, bmp))
+        im.save(join(dest, bmp),optimize=True,quality=65) 
+
 
 
 # gifsicle(
